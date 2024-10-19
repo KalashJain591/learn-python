4cdd54ada37dda1c32e2d1d600ce459f99f97ee4
Kalash Jain <kalashjain446@gmail.com>
2024-10-19 12:09:51 +0530

    inital commit

---
+++ Tutorial/Basics/hello_world.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+print("Hello, World 2!")
+
+# print("Hello, World!")
+#     print("Hello, World 2!") # IndentationError: unexpected indent
+
+"""
+This is the way to write a multi-line comment as python ignores string which is not assigned to any variable.
+"""
